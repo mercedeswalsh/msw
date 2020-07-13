dd4dcbdbc9d3c2fa77ba05accaab9b0340770b20
mercedeswalsh <mercedes.scott@live.com>
2020-07-13 06:56:08 -0700

    updating photo on home page

--- client/src/components/PhotoCard/PhotoCard.js
+++ client/src/components/PhotoCard/PhotoCard.js
@@ -5,7 +5,6 @@ import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardActionArea from '@material-ui/core/CardActionArea'
 // image
-import sfswing from './images/sfswing.jpeg'
 import afterlight from './images/afterlight.jpg'
 
 const useStyles = makeStyles((theme) => ({
@@ -16,25 +15,29 @@ const useStyles = makeStyles((theme) => ({
             flex: '1 0 auto'
         },
         cover: {
-            width: 151,
+            width: 151
+        }
         },
+    image: {
+        width: '100%',
     }
 }))
 
 const PhotoCard = () => {
     const classes = useStyles()
-    const theme = useTheme()
 
     return (
         <Card className={classes.root}>
         <CardActionArea>
             <CardMedia
+            className={classes.image}
             component="img"
             alt="san francisco swing"
             height="225"
             image={afterlight}
             title="san francisco swing"
             />
+            
         </CardActionArea>
       </Card>
     )
